4a5d54bf9c4e9a0d35e084cde6e6346a6c782408
cosmozhang1995 <zjzxz3006@126.com>
2014-08-18 09:22:11 +0800

    Change config

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -24,7 +24,7 @@ return array(
 	'DB_TYPE'		=> 'mysql',				// 数据库类型
 	'DB_HOST'		=> 'localhost',		// 服务器地址
 	'DB_NAME'		=> 'cosmozhang',	// 数据库名
-	'DB_USER'		=> 'root',				// 用户名
+	'DB_USER'		=> 'httpd',				// 用户名
 	'DB_PWD'		=> 'ykoTh1QE',		// 密码
 	'DB_PORT'		=> 3306,					// 端口
 	'DB_PREFIX'	=> 'think_',			// 数据库表前缀 
