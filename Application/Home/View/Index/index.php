27514a30e402894c9ffdd9f79bc31c945111fdb2
cosmozhang1995 <zjzxz3006@126.com>
2014-08-26 15:20:09 +0800

    Baidu verification meta

--- Application/Home/View/Index/index.php
+++ Application/Home/View/Index/index.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="baidu-site-verification" content="ZktHpo90lB" />
 <title>花栗鼠与美女猫</title>
 <include file='Public/lib_req/css.php' />
 <link rel="stylesheet/less" type="text/css" href="__ROOT____CSS__/style.css">
